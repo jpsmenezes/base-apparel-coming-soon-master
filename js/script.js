3afdf6ccef44d070e88b31d9baebfe1f57370477
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2021-12-23 10:24:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-let erro = document.querySelector('#erro');
-document.querySelector('#email').addEventListener('blur', event => {
-       if(!email.event){
-              erro.innerHTML = "Email Inválido";
-       }
-});
+
