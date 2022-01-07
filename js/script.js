811cde287555e7b3dab20a32c299c3ca0a8b3f5a
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-01-07 18:36:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+let validator = {
+       handleSubmit: (event) => {
+              event.preventDefault(); // não enviar formulário
+              
+              let send = true;
+              let input = form.querySelector('input');
+              let check = validator.checkInput(input);
+              if (check !== true) {
+                     send = false;
+                     validator.showError(input, check);          
+              }
+              else if (send) {
+                     form.submit();
+              }
+        },
+        checkInput: (input) => {
+              
+        }
+}
 
+const form = document.querySelector('form');
+form.addEventListener('submit', validator.handleSubmit);
